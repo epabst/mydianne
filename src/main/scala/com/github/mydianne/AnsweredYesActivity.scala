51b87a9661eb5187ae6f8ba6a99b8395a0adfb5b
Eric Pabst <epabst@gmail.com>
2011-09-10 23:10:34 -0600

    Added a message if she answers yes, and a video if she answers no.

---
+++ src/main/scala/com/github/mydianne/AnsweredYesActivity.scala
@@ -0,0 +1,11 @@
+package com.github.mydianne
+
+import android.app.Activity
+import android.os.Bundle
+
+class AnsweredYesActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.answered_yes)
+  }
+}
--- src/main/scala/com/github/mydianne/QuestionActivity.scala
+++ src/main/scala/com/github/mydianne/QuestionActivity.scala
@@ -2,10 +2,23 @@ package com.github.mydianne
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.net.Uri
 
 class QuestionActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.question)
+  }
+
+  def answeredYes(view: View) {
+    val intent = new Intent(Intent.ACTION_VIEW)
+    intent.setClass(this, classOf[AnsweredYesActivity])
+    startActivity(intent)
+  }
+
+  def answeredNo(view: View) {
+    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=qS7nqwGt4-I")))
   }
 }
