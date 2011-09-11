aa90d58ed17b29858657bb8a6a63272ece45626c
Eric Pabst <epabst@gmail.com>
2011-09-10 21:44:41 -0600

    Created MyDianne app.

---
+++ src/main/scala/com/github/mydianne/QuestionActivity.scala
@@ -0,0 +1,11 @@
+package com.github.mydianne
+
+import android.app.Activity
+import android.os.Bundle
+
+class QuestionActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.question)
+  }
+}
